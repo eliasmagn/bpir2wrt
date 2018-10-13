9efbd4135b548db78657f4fd878c58921090857d
eliasmagn <elias@eliashisch.de>
2018-10-13 12:27:51 +0200

    Initial Commit

--- target/linux/mediatek/image/mt7623.mk
+++ target/linux/mediatek/image/mt7623.mk
@@ -1,6 +1,13 @@
 define Device/7623n-bananapi-bpi-r2
   DEVICE_TITLE := MTK7623n BananaPi R2
   DEVICE_DTS := mt7623n-bananapi-bpi-r2
+  DEVICE_PACKAGES := wmt uboot-mtk-bpi-r2 kmod-crypto-hw-mtk kmod-nat-hw-mtk kmod-mt6625l-wlan-gen2 \
+	u-boot-bpi_r2 uboot-envtools attr f2fs-tools f2fsck mkf2fs 
+  SUPPORTED_DEVICES := bananapi,bpi-r2
+  IMAGES := sysupgrade.tar sysupgrade-sd.img sysupgrade-emmc.img
+  IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
+  IMAGE/sysupgrade-sd.img := sysupgrade-bpi-r2-sd
+  IMAGE/sysupgrade-emmc.img := sysupgrade-bpi-r2-emmc
 endef
 
 TARGET_DEVICES += 7623n-bananapi-bpi-r2
